094f851392db48ff696f4aca52a9763e67a22968
ngadjou soh tatiana merveille <tatianasoh5@gmail.com>
2021-07-21 10:36:08 +0100

    ajout du champ credit

--- subject.py
+++ subject.py
@@ -6,3 +6,4 @@ class UniversitySubject(models.Model):
      _name = 'university.subject'
      name = fields.Char('Name of Subject')
      code = fields.Char('Code of Subject')
+     credit = fields.Integer('Nombre de credits')
